f5e07944cf89ab711bc27f32698ad869adc72416
gngsn <rudtjs4540@gmail.com>
2020-07-07 19:54:49 +0900

    renewal portfolio

--- assets/js/skillChart.js
+++ assets/js/skillChart.js
@@ -1,5 +1,6 @@
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
+var langCtx = document.getElementById("language").getContext('2d');
+var textCtx = document.getElementById("tect").getContext('2d');
+var language = new Chart(langCtx, {
     type: 'bar',
     data: {
         labels: ["JavaScript", "TypeScript", "Java", "Swift", "C", "C++"],
@@ -12,8 +13,7 @@ var myChart = new Chart(ctx, {
                 'rgba(43, 43, 43, 1)',
                 'rgba(43, 43, 43, 1)',
                 'rgba(43, 43, 43, 1)'
-            ],
-            // borderWidth: 0
+            ]
         }]
     },
     options: {
@@ -27,7 +27,6 @@ var myChart = new Chart(ctx, {
         scales: {
             yAxes: [{
                 display: false,
-                barThickness: 10,
                 ticks: {
                     display: false,
                     beginAtZero: true
@@ -35,6 +34,54 @@ var myChart = new Chart(ctx, {
                 gridLines: {
                     display: false
                 }
+            },{
+                barThickness: 6,
+            }],
+            xAxes: [{
+                gridLines: {
+                    display: false,
+                    drawBorder: false,
+                }
+            }]
+        },
+    }
+});
+
+var textCtx = new Chart(textCtx, {
+    type: 'bar',
+    data: {
+        labels: ["Node", "Spring", "iOS", "React", "Angular"],
+        datasets: [{
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(43, 43, 43,1)',
+                'rgba(43, 43, 43, 1)',
+                'rgba(43, 43, 43, 1)',
+                'rgba(43, 43, 43, 1)',
+                'rgba(43, 43, 43, 1)'
+            ]
+        }]
+    },
+    options: {
+        maintainAspectRatio: true,
+        legend: {
+            display: false
+        },
+        tooltips: {
+            enabled: false
+        },
+        scales: {
+            yAxes: [{
+                display: false,
+                ticks: {
+                    display: false,
+                    beginAtZero: true
+                },
+                gridLines: {
+                    display: false
+                }
+            },{
+                barThickness: 6,
             }],
             xAxes: [{
                 gridLines: {
