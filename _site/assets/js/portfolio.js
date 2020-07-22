46f7e80afecbecd3d98366ea4eaa924ce7555377
gngsn <rudtjs4540@gmail.com>
2020-07-22 23:50:19 +0900

    Update portfolio

---
+++ _site/assets/js/portfolio.js
@@ -0,0 +1,113 @@
+$(document).ready(function () {
+    
+    var nav = $(".fixed-nav .nav li"); //버튼을 변수에 할당(저장)
+    var cont = $("#contents>section"); //컨텐츠를 변수에 할당
+
+    nav.click(function (e) {
+        var target = $(this); //클릭한 타겟을 변수에 할당
+        var index = target.index(); //클릭한 타겟에 번호를 할당
+        var section = cont.eq(index); //클릭한 버튼과 컨텐츠를 연결
+        var offset = section.offset().top; //각 컨텐츠의 오프셋 값을 할당
+        
+        $("html,body").animate({
+            scrollTop: offset
+        }, 1000);
+    });
+
+    $(window).scroll(function () {
+        var wScroll = $(this).scrollTop();
+        if (wScroll >= cont.eq(0).offset().top) {
+            nav.children().removeClass("active");
+            nav.eq(0).children().addClass("active");
+        }
+        if (wScroll >= cont.eq(1).offset().top) {
+            nav.children().removeClass("active");
+            nav.eq(1).children().addClass("active");
+        }
+        if (wScroll >= cont.eq(2).offset().top) {
+            nav.children().removeClass("active");
+            nav.children().eq(2).addClass("active");
+        }
+        if (wScroll >= cont.eq(3).offset().top) {
+            nav.children().removeClass("active");
+            nav.children().eq(3).addClass("active");
+        }
+    });
+});
+
+let email = "rudtjs4540@gmail.com";
+document.querySelector("#icon_mail").addEventListener("click", function(){
+    var emailCopy = document.createElement('textarea');
+    emailCopy.value = email;
+    document.body.appendChild(emailCopy);
+    emailCopy.select();
+    alert(email+"\n메일이 복사되었습니다.")
+    document.execCommand("copy");
+    document.body.removeChild(emailCopy);
+});
+
+
+var langCtx = document.getElementById("language").getContext('2d');
+var textCtx = document.getElementById("tect").getContext('2d');
+let options= {
+    maintainAspectRatio: true,
+    legend: {
+        display: false
+    },
+    tooltips: {
+        enabled: false
+    },
+    scales: {
+        yAxes: [{
+            display: false,
+            ticks: {
+                display: false,
+                beginAtZero: true
+            },
+            gridLines: {
+                display: false
+            }
+        }],
+        xAxes: [{
+            categoryPercentage: 1,
+            gridLines: {
+                display: false,
+                drawBorder: false,
+            },
+            barThickness: 22,
+            ticks: {
+                fontColor: "white",
+                fontSize: 12,
+                stepSize: 1,
+                beginAtZero: true
+            }
+        }]
+    },
+}
+var language = new Chart(langCtx, {
+    type: 'bar',
+    data: {
+        labels: ["Java", "JS", "SQL", "PHP", "C"],
+        datasets: [{
+            data: [80, 100, 100, 70, 60],
+            backgroundColor: [
+                '#fff','#fff','#fff','#fff','#fff'
+            ]
+        }]
+    },
+    options: options
+});
+  
+var textCtx = new Chart(textCtx, {
+    type: 'bar',
+    data: {
+        labels: ["Node", "Spring", "Linux", "Git", "MySQL"],
+        datasets: [{
+            data: [100, 80, 50, 100, 100],
+            backgroundColor: [
+                '#fff','#fff','#fff','#fff','#fff'
+            ]
+        }]
+    },
+    options: options
+});
